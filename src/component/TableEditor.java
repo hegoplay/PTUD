8ad09934d3badbcae81a770cac92ed298e607f97
hegoplay <thmnh113@gmail.com>
2023-11-13 11:03:10 +0700

    adding linh into manh

---
+++ src/component/TableEditor.java
@@ -0,0 +1,10 @@
+package component;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JTextField;
+
+public class TableEditor extends DefaultCellEditor{
+    public TableEditor(JTextField textField) {
+        super(textField);
+    }
+}
--- src/dao/KhachHangDAO.java
+++ src/dao/KhachHangDAO.java
@@ -31,4 +31,27 @@ public class KhachHangDAO {
 		}
 		return kh;
 	}
+	public static KhachHang getKHBySDT(String sdt) {
+		KhachHang kh = null;
+		try {
+			Connection con = ConnectDB.getConection();
+			String sql = "Select maKH from KhachHang where sdt = ?";
+			PreparedStatement statement = con.prepareStatement(sql);
+			statement.setString(1, sdt);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				String maKH = rs.getString(1);
+				String tenKH = rs.getNString(2);
+				String diaChi = rs.getNString(3);
+				int namSinh = rs.getInt(5);
+				boolean gioiTinh = rs.getBoolean(6);
+				kh = new KhachHang(maKH,tenKH,diaChi,sdt,namSinh,gioiTinh);
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return kh;
+	}
 }
